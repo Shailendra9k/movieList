9d0c3b109e06bc13b5ea3089bd4edd76894144ae
Shailendra9k <learning4succedding@gmail.com>
2020-02-18 01:46:43 +0200

    added changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from "./containers/List"
 
 const App = () => {
-    return <h1>Movies List</h1>
+    return (
+        <>
+        <h1>Movies List</h1>
+        <List /></>
+    )
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
