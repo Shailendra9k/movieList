8fe1f6d893cfd075318e0fdcbbd51b80f47297cf
Shailendra9k <learning4succedding@gmail.com>
2020-02-18 02:42:49 +0200

    fetching data

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import Card from "../components/Card/Card"
 
 class List extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+            loading: true
+    
+        }
+    
+    }
+
+    async componentDidMount() {
+        const movies = await fetch('../../assets/data.json');
+        const moviesJSON = await movies.json();
+
+        if(moviesJSON) {
+            this.setState({
+                data: moviesJSON,
+                loading: false,
+            })
+        }
+    }
     render() {
         return (
             <div>
-                <Card />
+                
             </div>
         );
     }
