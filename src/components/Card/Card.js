7b7c2e66e38124e80e8e5aaeb4db122b3c3fc897
Shailendra9k <learning4succedding@gmail.com>
2020-02-18 02:26:12 +0200

    added changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({movie}) => {
     return (
         <div>
-            <h2>Movi #1</h2>
+            <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+            <img src={movie.img.src} alt={movie.img.alt} width='200' />
+            <p>{`Distributor: ${movie.distributor}`}</p>
+            <p>{`Amount: ${movie.amount}`}</p>
         </div>
     );
 };
